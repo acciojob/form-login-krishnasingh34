c9a220b96a18cb3e0b5ea6ddc640ffa8c2ef09ab
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-12 15:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+let form = document.querySelector('form')
+let fname = document.querySelector('#fname')
+let lname = document.querySelector('#lname')
+form.addEventListener('submit', getFormvalue)
 function getFormvalue(e) {
     //Write your code here
 	e.preventDefault()
             let firstname = fname.value
             let lastname = lname.value
             alert(firstname+lastname)
-
 }
-
-let form = document.querySelector('form')
-        let fname = document.querySelector('#fname')
-        let lname = document.querySelector('#lname')
-        form.addEventListener('submit', getFormvalue)
